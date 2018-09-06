9052aca8a9ddaf78dda02adfd86be02f8094365f
Julian Kulesh <juliankul@gmail.com>
2018-09-07 01:22:13 +0300

    move lightio to main file

--- lib/async4r.rb
+++ lib/async4r.rb
@@ -1,5 +1,38 @@
 require "async4r/version"
 
 module Async4r
-  # Your code goes here...
+  def self.included(klass)
+    klass.include(ClassMethods)
+    klass.extend(self)
+  end
+
+  def async(name)
+    original_method = instance_method(name)
+
+    remove_method(name)
+
+    define_method(name) do |*args|
+      lambda do
+        original_method.bind(self).call(*args)
+      end
+    end
+  end
+
+  module ClassMethods
+    def await(blocks)
+      if blocks.is_a? Array
+        blocks.each { |block| run_in_threadlet(block) }.each(&:join)
+      else
+        run_in_threadlet(block).join
+      end
+    end
+
+    private
+
+    def run_in_threadlet(block)
+      LightIO::Beam.new do
+        block.call
+      end
+    end
+  end
 end
